8900d6dcf3c0d54736104aa860c977a732ea0883
pyadrus <zh.vitaliy92@gmail.com>
2023-05-20 22:53:00 +0300

    Доработана функция удаления ссылок в группе / чате Telegram

--- system/sqlite.py
+++ system/sqlite.py
@@ -22,33 +22,6 @@ def writing_to_the_database_about_a_new_user(name_table, chat_id, chat_title, us
     conn.close()
 
 
-def record_the_id_of_allowed_users(chat_id, user_id, username, first_name, last_name, date_add, admin_id, chat_title):
-    """
-    Мы записываем идентификатор пользователя, которому будет разрешено выполнение определенных действий в чате,
-    в базу данных. Будут сохранены идентификаторы чата и участника чата:
-    chat_id - идентификатор чата, в котором пользователю будут предоставлены права;
-    user_id - идентификатор пользователя, которому будут предоставлены определенные права;
-    username - username пользователя
-    first_name - имя пользователя
-    last_name - фамилия пользователя
-    date_add - дата добавления идентификатора пользователя в базу данных;
-    admin_id - идентификатор администратора, который добавил пользователя в базу данных;
-    chat_title - название чата
-    """
-    with sqlite3.connect(path_database) as conn:
-        # Инициализация соединения с базой данных SQLite
-        cursor = conn.cursor()
-        # Создание таблицы пользователей, если ее еще нет
-        cursor.execute("""CREATE TABLE IF NOT EXISTS privileged_users 
-                       (chat_id, user_id,username, first_name,last_name, date_add, admin_id, chat_title)""")
-        # Записываем ID пользователя в базу данных
-        cursor.execute("INSERT INTO privileged_users "
-                       "(chat_id, user_id, username, first_name, last_name, date_add, admin_id, chat_title) "
-                       "VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (chat_id, user_id, username, first_name,
-                                                           last_name, date_add, admin_id, chat_title))
-        conn.commit()
-
-
 def reading_data_from_the_database():
     conn = sqlite3.connect(path_database)
     cursor = conn.cursor()
